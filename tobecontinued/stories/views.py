7a245a1659b733ced8a7648d4b8cce73a1376f61
Aviv Kaplan <aviv12@cs.huji.ac.il>
2013-07-01 17:04:59 +0300

    wrote the submitLine function

--- tobecontinued/stories/views.py
+++ tobecontinued/stories/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.shortcuts import render
+from stories.models import Line, Story
 
 def homePage(request):
     context = {}
@@ -7,6 +8,9 @@ def homePage(request):
 
 def submitLine(request):
     sentence = request.POST['sentence']
+    s = Story.objects.filter(title__startswith = "Sadek")
+    Line(content = sentence, story = s[0]).save()
+    return HttpResponseRedirect('story')
     
 def storyline(request):
     context={}
