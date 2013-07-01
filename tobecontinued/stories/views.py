7efc719d9c929da19c05fdd5dfe6a6a6a2f53e88
Hind Elkurd <hind12@cs.huji.ac.il>
2013-07-01 10:44:57 +0300

    Changed urls.py and views.py

--- tobecontinued/stories/views.py
+++ tobecontinued/stories/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def homePage(request):
     context = {}
     return render(request, 'stories/HomePage.html', context)
+
+def storyline(request):
+    context={}
+    return render(request,'stories/storyline.html',context)
+
