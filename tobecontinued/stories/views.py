f3f300cda85396e3a6838562eb057c57553cffcf
Revital Gornstein <revital12@cs.huji.ac.il>
2013-07-03 12:45:28 +0300

    debugged

--- tobecontinued/stories/views.py
+++ tobecontinued/stories/views.py
@@ -9,21 +9,22 @@ def homePage(request):
 
 def create(request):
     context = {}
-    return render(request, 'creatstory.html', context)
+    return render(request, 'stories/createstory.html', context)
 
 def submitLine(request, storyID):
     sentence = request.POST['sentence']
     s = Story.objects.filter(id = storyID)
     Line(content = sentence, story = s[0]).save()
-    return HttpResponseRedirect('story/' + storyID)
+    return HttpResponseRedirect('story/' + str(storyID))
 
 def storyline(request, storyID):
     stories = Story.objects.filter(id = storyID)
     list1 = ['Story Not Found']
     context = {'Lines':list1}
     if stories != []:
+        s = stories[0]
 	lines = Line.objects.filter(story = stories[0])
-    	context={'Lines':lines}	
+    	context={'Lines':lines, 'storyTitle':s.title}	
     return render(request,'stories/storyline.html',context)
 
 
@@ -33,12 +34,12 @@ def storyline(request, storyID):
   #  return HttpResponseRedirect('story')
 
 def newStory(request):
-    title1 = request.POST['title']
-    firstLine = request.Post['firstLine']
+    title1 = request.POST['Title']
+    firstLine = request.POST['firstLine']
     new = Story(title = title1)
     new.save()
     Line(content = firstLine, story = new).save()
-    return HttpResponseRedirect('story/' + new.id)
+    return HttpResponseRedirect('story/' + str(new.id))
 
 #def newStory(request):
   #  newStory = request.POST['newStory']
