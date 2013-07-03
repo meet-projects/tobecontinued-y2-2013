90d9451dcae431d92214553119f4019c9ab2a251
Mai Stern <maistern@cs.huji.ac.il>
2013-07-03 15:29:18 +0300

    added signup page

--- tobecontinued/stories/views.py
+++ tobecontinued/stories/views.py
@@ -49,3 +49,6 @@ def newStory(request):
     
 def profile(request):
     return render(request, 'stories/profile.html', {})
+
+def signup(request):
+    return render(request, 'stories/signuppage.html', {})
